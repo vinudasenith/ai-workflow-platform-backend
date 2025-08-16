430191e4d2178c456b06f780b1840545747f6e8c
vinudasenith <ha.vinudas@gmail.com>
2025-08-16 21:14:55 +0530

    add organization service file✔️

---
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/service/OrganizationService.java
@@ -0,0 +1,50 @@
+package com.vinuda.workflowplatform.ai_workflow_platform.service;
+
+import com.vinuda.workflowplatform.ai_workflow_platform.model.Organization;
+import com.vinuda.workflowplatform.ai_workflow_platform.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    public Organization registerOrganization(Organization organization) {
+        List<Organization> existingOrgs = organizationRepository.findByTenantId(organization.getTenantId());
+
+        boolean exists = false;
+        for (Organization org : existingOrgs) {
+            if (org.getName().equals(organization.getName())) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists) {
+            return null;
+        }
+        return organizationRepository.save(organization);
+
+    }
+
+    public List<Organization> getOrganizationsByTenantId(String tenantId) {
+        return organizationRepository.findByTenantId(tenantId);
+    }
+
+    public Organization getOrganizationById(String id) {
+        return organizationRepository.findById(id).orElse(null);
+    }
+
+    public boolean deleteOrganization(String id) {
+        if (organizationRepository.existsById(id)) {
+            organizationRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
