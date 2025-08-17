df3231cfac1b8b88c549ca1139c8fa246e7948f1
vinudasenith <ha.vinudas@gmail.com>
2025-08-17 17:41:18 +0530

    implement Task model✔️

---
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/model/Task.java
@@ -0,0 +1,30 @@
+package com.vinuda.workflowplatform.ai_workflow_platform.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "tasks")
+
+public class Task {
+
+    @Id
+    private String id;
+
+    private String workflowId;
+    private String name;
+    private String description;
+
+    private LocalDateTime createdAt;
+    private TaskStatus status;
+
+    public enum TaskStatus {
+        PENDING, IN_PROGRESS, COMPLETED, REJECTED
+    }
+}
