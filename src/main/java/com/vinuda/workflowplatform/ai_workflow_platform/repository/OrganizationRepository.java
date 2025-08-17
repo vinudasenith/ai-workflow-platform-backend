5bddc779ed81117be6085f8f0354cf2787aefae7
vinudasenith <ha.vinudas@gmail.com>
2025-08-17 12:10:34 +0530

    remove unused imports✅

--- src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/repository/OrganizationRepository.java
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/repository/OrganizationRepository.java
@@ -3,9 +3,6 @@ package com.vinuda.workflowplatform.ai_workflow_platform.repository;
 import com.vinuda.workflowplatform.ai_workflow_platform.model.Organization;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 public interface OrganizationRepository extends MongoRepository<Organization, String> {
