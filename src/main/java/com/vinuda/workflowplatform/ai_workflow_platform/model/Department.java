e367b1adf3e82160f344ce9c50f2fd80ef1ba961
vinudasenith <ha.vinudas@gmail.com>
2025-08-17 12:10:58 +0530

    add Department model✌️

---
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/model/Department.java
@@ -0,0 +1,23 @@
+package com.vinuda.workflowplatform.ai_workflow_platform.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "departments")
+
+public class Department {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String organizationId;
+    private String description;
+
+}
