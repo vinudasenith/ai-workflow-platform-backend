6d5e37344aca1e66a1237dec86be5ff31bbacc4e
vinudasenith <ha.vinudas@gmail.com>
2025-08-17 17:41:34 +0530

    create Task repository👍

---
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/repository/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.vinuda.workflowplatform.ai_workflow_platform.repository;
+
+import com.vinuda.workflowplatform.ai_workflow_platform.model.Task;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+    // Find all tasks for a given workflow
+    List<Task> findByWorkflowId(String workflowId);
+
+    // Find tasks by status
+    List<Task> findByStatus(Task.TaskStatus status);
+
+}
