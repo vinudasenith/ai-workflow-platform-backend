e4e876c1e70ac257f800d277bec2a61f05e60b20
vinudasenith <ha.vinudas@gmail.com>
2025-08-18 19:06:20 +0530

    add repo method to find organization by owner email✅

--- src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/repository/OrganizationRepository.java
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/repository/OrganizationRepository.java
@@ -4,8 +4,11 @@ import com.vinuda.workflowplatform.ai_workflow_platform.model.Organization;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrganizationRepository extends MongoRepository<Organization, String> {
     List<Organization> findByTenantId(String tenantId);
+
+    Optional<Organization> findByOwnerEmail(String ownerEmail);
 
 }
