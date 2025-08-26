f68f997c034de7cdfd4041fe299feeb3651e5d07
vinudasenith <ha.vinudas@gmail.com>
2025-08-26 17:29:57 +0530

    add getTasksByTenantId method,Comment out updateTaskStatus method✌️

--- src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/service/TaskService.java
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/service/TaskService.java
@@ -2,6 +2,9 @@ package com.vinuda.workflowplatform.ai_workflow_platform.service;
 
 import com.vinuda.workflowplatform.ai_workflow_platform.model.Task;
 import com.vinuda.workflowplatform.ai_workflow_platform.repository.TaskRepository;
+
+import com.vinuda.workflowplatform.ai_workflow_platform.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +18,7 @@ public class TaskService {
 
     // Create a new task
     public Task createTask(Task task) {
-        task.setStatus(Task.TaskStatus.PENDING); // default status
+        // task.setStatus(Task.TaskStatus.PENDING); // default status
         return taskRepository.save(task);
     }
 
@@ -24,20 +27,24 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    // Update task status
-    public Task updateTaskStatus(String taskId, Task.TaskStatus newStatus) {
-        Task task = taskRepository.findById(taskId).orElse(null);
-        if (task == null) {
-            throw new RuntimeException("Task not found with id: " + taskId);
-        }
-        task.setStatus(newStatus);
-        Task updatedTask = taskRepository.save(task);
-        return updatedTask;
-    }
+    // // Update task status
+    // public Task updateTaskStatus(String taskId, Task.TaskStatus newStatus) {
+    // Task task = taskRepository.findById(taskId).orElse(null);
+    // if (task == null) {
+    // throw new RuntimeException("Task not found with id: " + taskId);
+    // }
+    // task.setStatus(newStatus);
+    // Task updatedTask = taskRepository.save(task);
+    // return updatedTask;
+    // }
 
     // Delete task
     public void deleteTask(String id) {
         taskRepository.deleteById(id);
+    }
+
+    public List<Task> getTasksByTenantId(String tenantId) {
+        return taskRepository.findByTenantId(tenantId);
     }
 
 }
