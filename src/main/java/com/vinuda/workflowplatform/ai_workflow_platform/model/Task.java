defad2e4f2df1153bd18b75554f10ca942e38b71
vinudasenith <ha.vinudas@gmail.com>
2025-08-27 17:35:39 +0530

    add to store multiple file urls👍

--- src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/model/Task.java
+++ src/main/java/com/vinuda/workflowplatform/ai_workflow_platform/model/Task.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -28,4 +30,6 @@ public class Task {
     // public enum TaskStatus {
     // PENDING, IN_PROGRESS, COMPLETED, REJECTED
     // }
+
+    private List<String> fileUrls = new ArrayList<>();
 }
